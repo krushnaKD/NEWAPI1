2ff5cdeee6108c92746ff7c3b4b23dae03ac72d4
krushna ankade <ankadekrushna01@gmail.com>
2024-08-26 15:28:32 +0530

    c

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+const connectDB = require("./db/connect");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const products_routes=require("./routes/products");
+app.get("/",(req,res)=>{
+    res.send("HI ,I am Live")
+})
+
+app.use("/api/products",products_routes);
+
+const start = async ()=>{
+    try{
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(PORT , ()=>{
+            
+            console.log(`${PORT} Yes Iam connnecteed`)
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+start()
